f5b9e1f24bff48cfa1ce0e607b010c33115b4fb4
Sanjay <sanjaykumarv.s05@gmail.com>
2026-04-17 18:53:06 +0530

    API_URL Integration

--- sacs-ryla/js/app.js
+++ sacs-ryla/js/app.js
@@ -208,4 +208,13 @@
     init();
   }
 
+  const API_URL = "https://script.google.com/macros/s/AKfycbzb--cocPl8aZbJjXC8UA4VkDT3AdBm4Za-qqS0O7jLn6H-PkmMA_i78Cxj4egwr7lvnQ/exec";
+  
+  async function saveToBackend() {
+    await fetch(API_URL, {
+      method: "POST",
+      body: JSON.stringify(collectData())
+    });
+  }
+
 })();
